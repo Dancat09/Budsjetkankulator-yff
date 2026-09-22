60a5c8defc57e45ce174330938bb7df2849ae1fd
Dancat09 <davoa006@osloskolen.no>
2026-09-22 09:09:42 +0200

    Main commit
    Jeg har funnet mest av funksjonene på w3schools og fant de jeg trenger og har legget det inn nå har jeg leget hvis du trikker på income som er inntekt og expenses at det er utekter og total på bege av de delene og selve ballasnsen til sammen men har ikke blit enda ferdig så jeg jobber med det videre, til alt skal fungere

--- javascript/script.js
+++ javascript/script.js
@@ -51,3 +51,75 @@ function addExpense() {
     saveBudget();
     showBudget();
 } 
+function showBudget() {
+ 
+    let list = document.getElementById("list");
+ 
+    list.innerHTML = "";
+ 
+    let incomeTotal = 0;
+    let expenseTotal = 0;
+ 
+    for (let i = 0; i < incomes.length; i++) {
+ 
+        incomeTotal += incomes[i].amount;
+ 
+        list.innerHTML += `
+            <div class="post income">
+                <div>
+                    <strong>${incomes[i].name}</strong>
+                    <br>
+                    ${incomes[i].category}
+                    <br>
+                    + ${money(incomes[i].amount)}
+                </div>
+ 
+                <button onclick="deleteIncome(${i})">
+                    Slett
+                </button>
+            </div>
+        `;
+    }
+ 
+    for (let i = 0; i < expenses.length; i++) {
+ 
+        expenseTotal += expenses[i].amount;
+ 
+        list.innerHTML += `
+            <div class="post expense">
+                <div>
+                    <strong>${expenses[i].name}</strong>
+                    <br>
+                    ${expenses[i].category}
+                    <br>
+                    - ${money(expenses[i].amount)}
+                </div>
+ 
+                <button onclick="deleteExpense(${i})">
+                    Slett
+                </button>
+            </div>
+        `;
+    }
+ 
+    if (incomes.length == 0 && expenses.length == 0) {
+        list.innerHTML = "<p>Ingen poster lagt til.</p>";
+    }
+ 
+    let balance = incomeTotal - expenseTotal;
+ 
+    document.getElementById("incomeTotal").textContent =
+        money(incomeTotal);
+ 
+    document.getElementById("expenseTotal").textContent =
+        money(expenseTotal);
+ 
+    document.getElementById("balance").textContent =
+        money(balance);
+ 
+    if (balance < 0) {
+        document.getElementById("balance").classList.add("minus");
+    } else {
+        document.getElementById("balance").classList.remove("minus");
+    }
+} 
